b3acf6b2e7af2304e7395f9e20434f2ce5b96720
Hedi Guellouz <hediguellouz@hotmail.fr>
2024-08-12 10:25:35 +0200

    -Display and Update BookDetails

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -69,6 +69,42 @@ namespace CodingWiki_Web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            BookDetail obj = new();
+
+            obj.Book = _db.Books.FirstOrDefault(u => u.Book_Id == id);
+            obj = _db.BookDetails.FirstOrDefault(u => u.Book_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(BookDetail obj)
+        {
+            if (obj.BookDetail_Id == 0)
+            {
+                //create
+                await _db.BookDetails.AddAsync(obj);
+            }
+            else
+            {
+                //update
+                _db.BookDetails.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         public async Task<IActionResult> Delete(int id)
         {
             Book obj = new();
