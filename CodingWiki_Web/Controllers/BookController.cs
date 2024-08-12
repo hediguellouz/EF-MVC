f28721d961ac622812e979c73a67cc512ec0458c
Hedi Guellouz <hediguellouz@hotmail.fr>
2024-08-12 10:31:50 +0200

    -User Eager Loading instead of Explicit Loading

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using CodingWiki_Model.Models;
 using CodingWiki_Model.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodingWiki_Web.Controllers
 {
@@ -16,15 +17,15 @@ namespace CodingWiki_Web.Controllers
         }
         public IActionResult Index()
         {
-            List<Book> books = _db.Books.ToList();
-            foreach (var book in books)
-            {
-                //Least efficient
-                //book.Publisher = _db.Publishers.Find(book.Publisher_Id);
+            List<Book> books = _db.Books.Include(u => u.Publisher).ToList();
+            //foreach (var book in books)
+            //{
+            //    //Least efficient
+            //    //book.Publisher = _db.Publishers.Find(book.Publisher_Id);
                 
-                //More efficient
-                _db.Entry(book).Reference(u => u.Publisher).Load();
-            }
+            //    //More efficient
+            //    _db.Entry(book).Reference(u => u.Publisher).Load();
+            //}
             return View(books);
         }
 
@@ -78,8 +79,8 @@ namespace CodingWiki_Web.Controllers
 
             BookDetail obj = new();
 
-            obj.Book = _db.Books.FirstOrDefault(u => u.Book_Id == id);
-            obj = _db.BookDetails.FirstOrDefault(u => u.Book_Id == id);
+            obj = _db.BookDetails.Include(u=> u.Book).FirstOrDefault(u => u.Book_Id == id);
+
             if (obj == null)
             {
                 return NotFound();
